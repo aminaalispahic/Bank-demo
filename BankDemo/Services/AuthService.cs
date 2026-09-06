0a30640386f3abba87081e519415bdaa68bcc286
aalispahic5 <aalispahic1@etf.unsa.ba>
2026-09-06 13:53:55 +0200

    test: uvodim namjerni hardkodovani API kljuc (MTTR demo, run 2)

--- BankDemo/Services/AuthService.cs
+++ BankDemo/Services/AuthService.cs
@@ -9,16 +9,17 @@ namespace BankDemo.Services
     {
         private readonly IUserRepository _userRepository;
         private readonly ITokenService _tokenService;
+        
         // VULN: Hardkodirana tajna direktno u kodu
-   
+        private const string BackupAdminApiKey = "sk_live_51HqA8xJ2eZvKYlo3nQpR9tUvW7mN4bXc";
 
-        private readonly string? _backupAdminApiKey;
+       
 
         public AuthService(IUserRepository userRepository, ITokenService tokenService, IConfiguration configuration)
         {
             _userRepository = userRepository;
             _tokenService = tokenService;
-            _backupAdminApiKey = configuration["Stripe:ApiKey"];
+           
         }
 
         public async Task<RegisterResult> RegisterAsync(RegisterRequestDto request)
